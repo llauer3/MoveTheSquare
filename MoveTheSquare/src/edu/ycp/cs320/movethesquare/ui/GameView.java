5624c64db298fb63fbebe4c7a34190f1b78b08c8
Levi Lauer <llauer3@ycp.edu>
2015-02-20 13:27:25 -0500

    make the circle into a square this corresponds better to the game concept

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -65,7 +65,7 @@ public class GameView extends JPanel {
 
 		Square square = model.getSquare();
 		
-		g.fillOval((int) square.getX(), (int) square.getY(), (int) square.getWidth(), (int) square.getHeight());
+		g.fillRect((int) square.getX(), (int) square.getY(), (int) square.getWidth(), (int) square.getHeight());
 	}
 	
 	public static void main(String[] args) {
