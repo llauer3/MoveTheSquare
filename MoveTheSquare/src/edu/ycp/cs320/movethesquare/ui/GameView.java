f02ba9790d61d705dddb6cdbb60965dae08c6948
Levi Lauer <llauer3@ycp.edu>
2015-02-20 13:29:55 -0500

    change color to red

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -61,7 +61,7 @@ public class GameView extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g); // paint background
 		
-		g.setColor(Color.GREEN);
+		g.setColor(Color.RED);
 
 		Square square = model.getSquare();
 		
